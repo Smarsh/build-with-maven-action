4ad6f82d6d5d3cad8529ad668093b960aff40448
Scott Paige <scottpaige1@gmail.com>
2020-03-31 09:45:32 -0700

    First commit

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM zenika/alpine-maven:3-jdk8
+
+RUN apk update --no-cache && apk add gettext bash
+
+COPY task.sh /tash.sh
+
+CMD ["sh", "/task.sh"]
---
+++ task.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+credhub login --skip-tls-validation
+
+export ARTIFACTORY_USERNAME="$(credhub get -q -n /concourse/${CONCOURSE_TEAM}/artifactory-username)"
+export ARTIFACTORY_PASSWORD="$(credhub get -q -n /concourse/${CONCOURSE_TEAM}/artifactory-password)"
+
+envsubst < settings.xml > temp.xml
+
+rm settings.xml
+
+mv temp.xml settings.xml
+
+mvn clean test -s settings.xml
