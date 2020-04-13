aa65d62bb48552567c2c981f9d7d3b3f9ac5eea5
Scott Paige <scottpaige1@gmail.com>
2020-04-13 10:34:42 -0700

    Debugging

--- task.sh
+++ task.sh
@@ -5,11 +5,14 @@ credhub login --skip-tls-validation
 export ARTIFACTORY_USERNAME="$(credhub get -q -n /concourse/${CONCOURSE_TEAM}/artifactory-username)"
 export ARTIFACTORY_PASSWORD="$(credhub get -q -n /concourse/${CONCOURSE_TEAM}/artifactory-password)"
 
+
+echo $PWD
+
 ls -l
 
-ls ../ -l
-
-ls ../../ -l
+ls ../workflow -l
+ls ../workspace -l
+ls ../home -l 
 
 envsubst < ../../_actions/smarsh/build-with-maven-action/development/settings.xml > temp.xml
 
