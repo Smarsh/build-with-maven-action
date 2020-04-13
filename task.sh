960fd9a2d76911e0c0c64d68e47151800d72e7a7
Scott Paige <scottpaige1@gmail.com>
2020-04-13 10:08:58 -0700

    [DEL-1362] Updated settings.xml and task script

--- task.sh
+++ task.sh
@@ -5,7 +5,7 @@ credhub login --skip-tls-validation
 export ARTIFACTORY_USERNAME="$(credhub get -q -n /concourse/${CONCOURSE_TEAM}/artifactory-username)"
 export ARTIFACTORY_PASSWORD="$(credhub get -q -n /concourse/${CONCOURSE_TEAM}/artifactory-password)"
 
-envsubst < settings.xml > temp.xml
+envsubst < ../../_actions/smarsh/build-with-maven-action/development/settings.xml > temp.xml
 
 rm settings.xml
 
