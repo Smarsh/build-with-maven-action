888422b7e4cf13c5f4746eeca665791d238344cd
Scott Paige <scottpaige1@gmail.com>
2020-04-13 11:15:39 -0700

    [DEL-1389] removed debugging logic from task script

--- task.sh
+++ task.sh
@@ -5,13 +5,6 @@ credhub login --skip-tls-validation
 export ARTIFACTORY_USERNAME="$(credhub get -q -n /concourse/${CONCOURSE_TEAM}/artifactory-username)"
 export ARTIFACTORY_PASSWORD="$(credhub get -q -n /concourse/${CONCOURSE_TEAM}/artifactory-password)"
 
-
-echo $PWD
-
-ls -l
-
-ls ../../ -l
-
 envsubst < ../../settings.xml > temp.xml
 
 rm settings.xml
